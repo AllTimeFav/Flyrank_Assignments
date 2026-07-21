6bbb5fd6cb6af376021e95c229412881146e1a33
Abuzar Khan <mname6616@gmail.com>
2026-07-21 21:24:43 +0500

    Stage 2: read endpoints with 404

--- a1_crud/app/api/endpoints/tasks.py
+++ a1_crud/app/api/endpoints/tasks.py
@@ -1,9 +1,24 @@
+from fastapi import status
+from fastapi import HTTPException
 from fastapi import APIRouter
 
 router = APIRouter()
 
-tasks = []
+tasks = [
+    {"id": 1, "title": "First Book", "done": False},
+    {"id": 2, "title": "Second Book", "done": False},
+    {"id": 3, "title": "Third Book", "done": True}
+]
 
 @router.get("/")
 def get_tasks():
-    return { "name": "Task API", "version": "1.0", "endpoints": ["/tasks"] }
+    return {"tasks": tasks}
+
+@router.get("/{id}")
+def return_task(id: int):
+    for task in tasks:
+        if task["id"] == id:
+            return {"task": task}
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND, detail=f"Task {id} not found"
+    )
