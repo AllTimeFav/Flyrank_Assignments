7b1cdc126e0c51deeff28aad6eadde96135b86bd
Abuzar Khan <mname6616@gmail.com>
2026-08-17 01:01:50 +0500

    Stage 1: connect via .env and create table

--- a1_crud/app/main.py
+++ a1_crud/app/main.py
@@ -4,9 +4,17 @@ from fastapi import FastAPI
 from app.api.api import api_router
 from app.db.database import init_db
 
+import os
+from dotenv import load_dotenv
+from app.repositries.postgres_repo import PostgresRepository
+
+load_dotenv()
+POSTGRES_URL = os.getenv("DATABASE_URL")
+
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    init_db()
+    db = PostgresRepository(connection_url=POSTGRES_URL)
     yield
 
 app = FastAPI(
---
+++ a1_crud/app/repositries/postgres_repo.py
@@ -0,0 +1,32 @@
+import psycopg
+
+class PostgresRepository:
+    def __init__(self, connection_url: str):
+        self.connection_url = connection_url
+        self._init_db()
+
+    def _init_db(self):
+        with psycopg.connect(self.connection_url) as conn:
+            with conn.cursor() as cur:
+                # Create table if missing
+                cur.execute("""
+                    CREATE TABLE IF NOT EXISTS tasks (
+                        id SERIAL PRIMARY KEY,
+                        title TEXT NOT NULL,
+                        done BOOLEAN DEFAULT FALSE
+                    );
+                """)
+                
+                # Check row count
+                cur.execute("SELECT COUNT(*) FROM tasks;")
+                count = cur.fetchone()[0]
+                
+                # Seed only if table is empty
+                if count == 0:
+                    cur.execute("""
+                        INSERT INTO tasks (title, done) VALUES 
+                        ('Book 1', true),
+                        ('Book 2', false),
+                        ('Book 3', false);
+                    """)
+                conn.commit()
